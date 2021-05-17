b114f9a6eac0bf501967dcbb4c997ee7599db0f9
sharmilasharmila <sra084@post.uit.no>
2021-05-17 08:10:57 +0530

    3.4

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import axios from 'axios';
+
+import { MovieCard } from '../movie-card/movie-card';
+import { MovieView } from '../movie-view/movie-view';
+
+export class MainView extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      movies: [],
+      selectedMovie: null
+    }
+  }
+
+  componentDidMount(){
+    axios.get('https://sharmilamovie.herokuapp.com/movies')
+      .then(response => {
+        this.setState({
+          movies: response.data
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  setSelectedMovie(newSelectedMovie) {
+    this.setState({
+      selectedMovie: newSelectedMovie
+    });
+  }
+
+  render() {
+    const { movies, selectedMovie } = this.state;
+    if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
+    return (
+      <div className="main-view">
+        {selectedMovie
+          ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie) }}/>
+          : movies.map(movie => (
+            <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }}/>
+          ))
+        }
+      </div>
+    );
+  }
+}
---
+++ src/components/movie-view/movie-view.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+export class MovieView extends React.Component {
+
+  keypressCallback(event) {
+    console.log(event.key);
+  }
+
+  componentDidMount() {
+    document.addEventListener('keypress', this.keypressCallback);
+  }
+  
+  componentWillUnmount() {
+    document.removeEventListener('keypress', this.keypressCallback);
+  }
+
+  render() {
+    const { movie, onBackClick } = this.props;
+
+    return (
+      <div className="movie-view">
+        <div className="movie-poster">
+          <img src={movie.ImagePath} />
+        </div>
+        <div className="movie-title">
+          <span className="label">Title: </span>
+          <span className="value">{movie.Title}</span>
+        </div>
+        <div className="movie-description">
+          <span className="label">Description: </span>
+          <span className="value">{movie.Description}</span>
+        </div>
+        <button onClick={() => { onBackClick(null); }}>Back</button>
+       </div>
+    );
+  }
+}
